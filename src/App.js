79160e46e8362d3f6d3baf9c736835720b47bb63
NEETHU C <neethubalakrishna@gmail.com>
2023-01-31 19:13:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import Signup from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' exact element={<Home/>}/>
+        <Route path='/login' exact element={<Login/>}/>
+        <Route path='/navbar' exact element={<Navbar/>}/>
+        <Route path='/signup' exact element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
